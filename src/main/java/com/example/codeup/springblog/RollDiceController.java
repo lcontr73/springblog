338beab1f1181784361b7f50bb510cc468b06452
Leonard Contreras <leonard.contreras03@gmail.com>
2022-07-27 17:20:27 -0500

    views exercise passing data to views

---
+++ src/main/java/com/example/codeup/springblog/RollDiceController.java
@@ -0,0 +1,29 @@
+package com.example.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Random;
+
+@Controller
+public class RollDiceController {
+
+    @GetMapping("/roll-dice")
+    public String pickANumber() {
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{number}")
+    public String returnDiceRollResults(@PathVariable String number, Model vModel) {
+        Random rn = new Random();
+        int roll = (int)Math.floor(Math.random()*(6-1+1)) +1;
+        vModel.addAttribute("roll", roll);
+        vModel.addAttribute("number", number);
+        return "roll-dice-n";
+
+    }
+}
