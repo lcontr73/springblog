2e9849a29bd1fc110e9eab4ef3c6ebedce371cc7
Leonard Contreras <leonard.contreras03@gmail.com>
2022-08-04 09:55:25 -0500

    refactored to add styling

--- src/main/java/com/example/codeup/springblog/controller/HomeController.java
+++ src/main/java/com/example/codeup/springblog/controller/HomeController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
     @GetMapping("/")
-    @ResponseBody
+
     public String landing() {
-        return "This is the landing page!";
+        return "index";
     }
 }
